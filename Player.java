0aac23d11a3dd5607aa88ac7e717e7e22cdbbc5d
Dhruv Mangal <dhruv.mangal@nagarro.com>
2026-06-19 11:24:33 +0530

    ScoringApp-106: Added player object

---
+++ Player.java
@@ -0,0 +1,39 @@
+package com.cricket.scoring.model;
+
+import com.cricket.scoring.enums.PlayerRole;
+
+public class Player {
+
+    private String name;
+    private PlayerRole role;
+
+    // Batting stats
+    private int runs;
+    private int ballsFaced;
+
+    // Bowling stats
+    private int ballsBowled;
+    private int runsGiven;
+    private int wickets;
+
+    public Player(String name, PlayerRole role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    // Basic methods
+    public void addRuns(int runs) {
+        this.runs += runs;
+        this.ballsFaced++;
+    }
+
+    public void bowlBall(int runs, boolean isWicket) {
+        this.runsGiven += runs;
+        this.ballsBowled++;
+        if (isWicket) {
+            this.wickets++;
+        }
+    }
+
+    // Getters
+   }
